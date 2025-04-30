d418e43d43af0bfe4cff02a2fb142f8fa13375f5
el710 <el710@mail.ru>
2025-04-30 20:34:32 +0300

    docs: upgrade

---
+++ DJpro/CommU/uproject/tests/test_models_bases.py
@@ -0,0 +1,82 @@
+"""
+    Copyright (c) 2025 Kim Oleg <theel710@gmail.com>
+
+    
+    Test uproject/models/bases.py
+"""
+import unittest
+from unittest import TestCase
+import logging
+
+"""
+    To see neighbors directory
+"""
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent /"." ))
+
+from models.bases import UtemBase
+from models.uobject import UObject
+class TModuleBases(TestCase):
+    ## do use tests of that class
+    added = True
+
+    @classmethod
+    def setUpClass(cls): 
+        ## return super().setUpClass()
+        logging.debug(f"Start class testing ...")
+    
+    def setUp(self):
+        ## return super().setUp()
+        logging.debug("Start test... ")
+
+    def tearDown(self):
+        ## return super().tearDown()
+        logging.debug("test over")
+
+    @classmethod
+    def tearDownClass(cls):
+        ## return super().tearDownClass()
+        logging.debug(f"Class testing is over")
+
+    """
+        Tests
+    """
+
+    @unittest.skipIf(added == False, "Test <_test> is missed")
+    def test_base(self):
+        base = UtemBase()
+        self.assertIsNotNone(base, "Base create error")
+        item_1 = UObject("test1")
+        base.add(item_1)
+        self.assertIsNotNone(base.__next__(), "Add element to base error")
+        self.assertIsNotNone(base.read(item_1.get_token()), "Read base error")
+        item_2 = UObject("test2")
+        base.add(item_2, parent_id=item_1.get_token())
+        self.assertEqual(base.len(), 2, "Result of adding to base error")
+        for item in base:
+            self.assertIsInstance(item["utem"], UObject, "Wrong elements type")
+
+    """
+        template
+    """
+    @unittest.skipIf(added == False, "Test <_test> is missed")
+    def _test(self):
+        pass
+    """
+        self.assertEqual()
+        self.assertAlmostEqual()
+        self.assertNotEqual()
+        self.assertTrue()
+        self.assertIs()
+        self.assertIsNone(None)
+        self.assertIn()
+        self.assertRaises(expected_exception=TypeError)
+        self.assertWarns(expected_warning=Warning)
+        self.assertLess()
+        self.assertLessEqual()
+    """
+
+
+if __name__ == "__main__":   
+    unittest.main()
---
+++ DJpro/CommU/uproject/tests/test_template.py
@@ -0,0 +1,57 @@
+"""
+    Copyright (c) 2025 Kim Oleg <theel710@gmail.com>
+
+    
+    Test uproject/...
+"""
+import unittest
+import logging
+
+class Def(unittest.TestCase):
+    ## do use tests of that class
+    added = True
+
+    @classmethod
+    def setUpClass(cls): 
+        ## return super().setUpClass()
+        logging.debug(f"Start {cls.__class__()} testing ...")
+    
+    def setUp(self):
+        ## return super().setUp()
+        logging.debug("Start test ...")
+
+    def tearDown(self):
+        ## return super().tearDown()
+        logging.debug("test ... over")
+
+    @classmethod
+    def tearDownClass(cls):
+        ## return super().tearDownClass()
+        logging.debug(f"testing {cls.__class__()} is over")
+
+    """
+        Tests
+    """
+    @unittest.skipIf(added == False, "Test <_test> is missed")
+    def test_(self):
+        pass
+    """
+        self.assertEqual()
+        self.assertAlmostEqual()
+        self.assertNotEqual()
+        self.assertTrue()
+        self.assertIs()
+        self.assertIsNone(None)
+        self.assertIn()
+        self.assertRaises(expected_exception=TypeError)
+        self.assertWarns(expected_warning=Warning)
+        self.assertLess()
+        self.assertLessEqual()
+    """
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename=f"{__file__}_test.log", filemode="w", format="\n %(asctime)s | %(module)s | %(levelname)s | %(message)s",
+                        level=logging.DEBUG, encoding="utf-8")
+    
+    unittest.main()
