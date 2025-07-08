ecd95e79825666c57a52c9bd15f276feeb6d90a1
el710 <111958399+el710@users.noreply.github.com>
2025-07-08 20:13:16 +0300

    Delete CU_Project/DJproject/telegram/id_commu_bot.py

--- CU_Project/DJproject/telegram/id_commu_bot.py
+++
@@ -1,4 +0,0 @@
-EL_bot_name = "ElLittleBot" ##(@EL710Bot)
-EL_tel_token = "5565110850:AAEpoukwdaiq9uOELSLzrFQeZR54kIyWGU8"
-bot_name = "CommU_Tbot"
-tel_token = "7860249529:AAG7Y6vpfxSO1qRTY3a2jQrLNZt39e2BnAQ"
